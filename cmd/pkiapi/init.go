d13578747783b5aaf35260d67cc7742525d18b2d
Robert McLeod <robert@penguinpower.co.nz>
2021-04-20 23:48:25 +1200

    fix bugs with init, add extra protections, make it easier to use

--- cmd/pkiapi/init.go
+++ cmd/pkiapi/init.go
@@ -4,6 +4,7 @@ import (
 	"crypto/x509/pkix"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/opencoff/go-pki"
@@ -19,12 +20,14 @@ var initOpts = struct {
 	from    string
 }{}
 
+var defaultDBValidity uint = 5
+
 func init() {
 	// initCmd.Flags().StringVarP(&initOpts.from, "from-json", "j", "", "Initialize from an exported JSON dump")
 	initCmd.Flags().StringVarP(&initOpts.country, "country", "c", "US", "Use `C` as the country name")
 	initCmd.Flags().StringVarP(&initOpts.org, "organization", "O", "", "Use `O` as the organization name")
 	initCmd.Flags().StringVarP(&initOpts.ou, "organization-unit", "u", "", "Use `U` as the organization unit name")
-	initCmd.Flags().UintVarP(&initOpts.yrs, "validity", "V", 5, "Issue CA root cert with `N` years validity")
+	initCmd.Flags().UintVarP(&initOpts.yrs, "validity", "V", defaultDBValidity, "Issue CA root cert with `N` years validity")
 	rootCmd.AddCommand(initCmd)
 }
 
@@ -33,10 +36,26 @@ var initCmd = &cobra.Command{
 	Short: "init",
 	Long:  "init",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			log.Fatalf("ERROR: must specify common-name")
+		}
+
 		cn := args[0]
 
-		if len(cn) == 0 {
-			log.Fatalf("must specify common-name")
+		if password == "" {
+			log.Fatalf("ERROR: must specify password in PKI_PASSWORD or PKI_PASSWORD_FILE")
+		}
+
+		if dbfile == "" {
+			dbfile = "./" + cn + ".db"
+		}
+
+		if _, err := os.Stat(dbfile); err == nil {
+			log.Fatalf("ERROR: %s already exists", dbfile)
+		}
+
+		if initOpts.yrs == defaultDBValidity {
+			log.Printf("Using default validity: %d years (you can change this with -V)\n", initOpts.yrs)
 		}
 
 		p := pki.Config{
@@ -52,10 +71,11 @@ var initCmd = &cobra.Command{
 		}
 		ca, err := pki.New(&p, dbfile, true)
 		if err != nil {
-			log.Fatalf("failed to create PKI database: %s", err)
+			log.Fatalf("ERROR: failed to create PKI database at %s: %s", dbfile, err)
 		}
 
 		fmt.Printf("New CA cert:\n%s\n", pkiapi.Cert(*ca.Certificate))
+		log.Printf("created new PKI database at %s", dbfile)
 	},
 }
 
